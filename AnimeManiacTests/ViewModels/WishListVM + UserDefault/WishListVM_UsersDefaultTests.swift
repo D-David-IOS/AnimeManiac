b8e23b877a578b981039cfdef0c96af8cf277c3a
D-David-IOS <debehogne.david@gmail.com>
2022-02-05 11:39:51 -0800

    add tests for user defaults and wishList

--- AnimeManiacTests/ViewModels/WishListVM + UserDefault/WishListVM_UsersDefaultTests.swift
+++ AnimeManiacTests/ViewModels/WishListVM + UserDefault/WishListVM_UsersDefaultTests.swift
@@ -16,13 +16,40 @@ class WishListVM_UserDefault_Tests: XCTestCase {
     
     func testWishListVMWhen_EmptyUserDefaults_SoEmptyError(){
         
-        var anime = StateAnime.getAnimes()
+        // we delete all Animes with the key
+        UserDefaults.standard.removeObject(forKey: StateAnime.Constants.stateAnime.rawValue)
         
-        var wishListVM = WishListViewModel()
+        let wishListVM = WishListViewModel()
         
-        // Load datas with the animePage
-        AnimePageVM.loadData { error in
-            XCTAssertNil(error)
+        // Load datas with the animePage ( here : [] )
+        wishListVM.loadData { emptyError in
+            XCTAssertEqual(emptyError as! AnimeError, AnimeError.emptyWishList)
         }
     }
+    
+    func testWishListVMWhen_UserDefaultNotEmpty_SoNoError(){
+        
+        // save one fake StateAnime in UserDefault
+        StateAnime.saveAnime(stateAnime: [fakeData.stateAnime])
+        
+        let wishListVM = WishListViewModel()
+        
+        // Load datas with the animePage
+        wishListVM.loadData { emptyError in
+            XCTAssertNil(emptyError)
+            XCTAssertEqual(wishListVM.sections.count,1)
+        }
+        
+        // get all animes
+        let anime = StateAnime.getAnimes()
+        
+        // so we have our unique fake StateAnime in our UserDefault
+        XCTAssertEqual(anime.first?.title,"Fullmetal Alchemist: Brotherhood")
+        XCTAssertEqual(anime.first?.id ,"3936")
+        XCTAssertEqual(anime.first?.image ,"https://media.kitsu.io/anime/poster_images/3936/small.jpg")
+        XCTAssertEqual(anime.first?.synopsis ,"blabla i'm a synopsis")
+        XCTAssertEqual(anime.first?.inProgress ,false)
+        XCTAssertEqual(anime.first?.alreadySaw ,false)
+    }
+   
 }
